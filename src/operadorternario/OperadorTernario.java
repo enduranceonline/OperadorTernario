6955775ac7bf642d69818b89e6bd268f18954c8d
David <david.rodriguez.igual@gmail.com>
2024-12-01 17:08:29 +0100

    Commit inicial

---
+++ src/operadorternario/OperadorTernario.java
@@ -0,0 +1,23 @@
+package operadorternario;
+
+import java.util.Scanner;
+
+public class OperadorTernario {
+
+    public static void main(String[] args) {
+
+        /*programa que dependiendo del promedio de  un alumno, nos diga si aprobó o no una materia*/
+        //declaración de variables
+        double promedio;
+        String condicionFinal;
+        //Declarar Scanner que permita ingresar datos al sistema
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el promedio del alumno");
+        promedio = teclado.nextDouble();
+
+        condicionFinal = (promedio >= 6) ? "Aprobado" : "Desaprobado";
+        System.out.println("La condición final del alumno es " + condicionFinal + ", El promedio fue: " + promedio);
+
+    }
+
+}
